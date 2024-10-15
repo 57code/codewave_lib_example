3bad8a4a998fe8442fd4cbae14284e1378e579cd
57code <yt0379@sohu.com>
2024-10-15 18:52:39 +0800

    a

--- src/logics/index.ts
+++ src/logics/index.ts
@@ -20,3 +20,27 @@ export async function canIUse(apiName: nasl.core.String): Promise<nasl.core.Bool
     }
 }
 
+/**
+ * @NaslLogic
+ * @type both
+ * @title 测试pp接口可用性
+ * @desc 测试是否可以调用对应的pp接口
+ * @param title api名称
+ * @param durationType api名称
+ * @returns 返回布尔值
+ */
+export async function showToast(title: nasl.core.String, durationType): Promise<nasl.core.Boolean> {
+    try {
+        // 显示一个持续时间较长的 Toast
+        return await pp.showToast({ title: 'Hello POPO.', durationType: 2 });
+    } catch (err) {
+        console.error(err);
+        // catch or throw, this is a question
+        return false;
+    }
+}
+
+export function test(): nasl.core.String {
+    console.log("test");
+    return 'test'
+}

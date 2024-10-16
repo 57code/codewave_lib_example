7f8e7d5563454ee50f929a78dc4d0a74a1dcf15b
57code <yt0379@sohu.com>
2024-10-16 12:49:38 +0800

    atwho组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,13 @@
 import MyTestComp from './my-test-comp';
 import MyUButton from './my-u-button';
 import MyFullCalender from './my-full-calender';
+import AtWho from './at-who';
 // COMPONENT IMPORTS
 
 export {
     MyTestComp,
   MyUButton,
   MyFullCalender,
+  AtWho,
 // COMPONENT EXPORTS
 };
